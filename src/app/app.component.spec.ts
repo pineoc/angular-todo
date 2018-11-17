52431525cc3bacd280320a3494e1074a07557d28
pineoc <kingsuk92@gmail.com>
2018-11-17 17:43:10 +0900

    update app component
    - Added app component function
    - Added 'FormsModule' on app.module.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { Todo } from './todo';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        FormsModule
       ],
       declarations: [
         AppComponent
@@ -20,16 +21,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'todo-app'`, () => {
+  it(`should have a newTodo todo`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('todo-app');
-  });
+    expect(app.newTodo instanceof Todo).toBeTruthy();
+  }));
 
-  it('should render title in a h1 tag', () => {
+  it('should display "Todos" title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to todo-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Todos');
   });
 });
